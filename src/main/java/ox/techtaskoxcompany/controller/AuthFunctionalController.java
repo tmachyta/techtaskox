64d4114f28f52b442f9d3fafc344cc3078055dd1
Taras Machyta <taras.machyta.sa@gmail.com>
2024-12-21 23:17:54 +0200

    updates

--- src/main/java/ox/techtaskoxcompany/controller/AuthFunctionalController.java
+++ src/main/java/ox/techtaskoxcompany/controller/AuthFunctionalController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AuthFucntionalController {
+public class AuthFunctionalController {
     @GetMapping("/main-page")
     public String main() {
         return "mainpage";
